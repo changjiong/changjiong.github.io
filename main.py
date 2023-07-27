2918460ff5bd158d0643df346055122120ef17ad
ChangJiong <drunkwretch@gmail.com>
2023-07-27 02:04:40 +0000

    define newline

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ My personal blog using issues and GitHub Actions (随意转载，无需署名)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
-NEWLINE = NEWLINE
+NEWLINE = "\n"
 BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 5
 TOP_ISSUES_LABELS = ["Top"]
