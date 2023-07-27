7e810a1d7b0a1b85655dc7e37a808a364358adbf
drunkwretch <drunkwretch@gmail.com>
2023-07-27 22:53:14 +0800

    update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ TODO_ISSUES_LABELS = ["TODO"]
 FRIENDS_LABELS = ["Friends"]
 IGNORE_LABELS = FRIENDS_LABELS + TOP_ISSUES_LABELS + TODO_ISSUES_LABELS
 
-FRIENDS_TABLE_HEAD = "| Name | Link | Desc | \n | ---- | ---- | ---- |\n"
+FRIENDS_TABLE_HEAD = "| Name | Link | Desc | \n| ---- | ---- | ---- |\n"
 FRIENDS_TABLE_TEMPLATE = "| {name} | {link} | {desc} |\n"
 FRIENDS_INFO_DICT = {
     "名字": "",
@@ -282,7 +282,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
     add_md_tail("README.md")
-    markdown.markdownFromFile(input='README.md', output_format='html', extensions=['extra'])
+    markdown.markdownFromFile(input='README.md', output='README.md', encoding='utf-8')
 
     generate_rss_feed(repo, "feed.xml", me)
     to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
