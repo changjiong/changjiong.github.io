8a6ac2e4d8aab259c5b583aad85003872408d803
ChangJiong <drunkwretch@gmail.com>
2023-07-27 01:59:06 +0000

    update a new version update python 3.6 to 3.11

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
+import re
 
+from marko.ext.gfm import gfm as marko
 from github import Github
+from feedgen.feed import FeedGenerator
+from lxml.etree import CDATA
 
 MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions
+My personal blog using issues and GitHub Actions (随意转载，无需署名)
+[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
-NEWLINE = "\n"
+
+NEWLINE = NEWLINE
 BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 5
 TOP_ISSUES_LABELS = ["Top"]
 TODO_ISSUES_LABELS = ["TODO"]
+FRIENDS_LABELS = ["Friends"]
+IGNORE_LABELS = FRIENDS_LABELS + TOP_ISSUES_LABELS + TODO_ISSUES_LABELS
+
+FRIENDS_TABLE_HEAD = "| Name | Link | Desc | \n | ---- | ---- | ---- |\n"
+FRIENDS_TABLE_TEMPLATE = "| {name} | {link} | {desc} |\n"
+FRIENDS_INFO_DICT = {
+    "名字": "",
+    "链接": "",
+    "描述": "",
+}
+
 CSS = """<link rel="stylesheet" href="./style.css" />"""
 
 
@@ -19,9 +36,45 @@ def get_me(user):
     return user.get_user().login
 
 
-def isMe(issue, me):
+def is_me(issue, me):
     return issue.user.login == me
 
+
+def is_hearted_by_me(comment, me):
+    reactions = list(comment.get_reactions())
+    for r in reactions:
+        if r.content == "heart" and r.user.login == me:
+            return True
+    return False
+
+def _make_friend_table_string(s):
+    info_dict = FRIENDS_INFO_DICT.copy()
+    try:
+        string_list = s.splitlines()
+        # drop empty line
+        string_list = [l for l in string_list if l and not l.isspace()]
+        for l in string_list:
+            string_info_list = re.split("：", l)
+            if len(string_info_list) < 2:
+                continue
+            info_dict[string_info_list[0]] = string_info_list[1]
+        return FRIENDS_TABLE_TEMPLATE.format(
+            name=info_dict["名字"], link=info_dict["链接"], desc=info_dict["描述"]
+        )
+    except Exception as e:
+        print(str(e))
+        return
+
+# help to covert xml vaild string
+def _valid_xml_char_ordinal(c):
+    codepoint = ord(c)
+    # conditions ordered by presumed frequency
+    return (
+        0x20 <= codepoint <= 0xD7FF
+        or codepoint in (0x9, 0xA, 0xD)
+        or 0xE000 <= codepoint <= 0xFFFD
+        or 0x10000 <= codepoint <= 0x10FFFF
+    )
 
 def format_time(time):
     return str(time)[:10]
@@ -89,11 +142,11 @@ def add_md_todo(repo, md, me):
         for issue in todo_issues:
             if isMe(issue, me):
                 todo_title, todo_list = parse_TODO(issue)
-                md.write("TODO list from " + todo_title + "\n")
+                md.write("TODO list from " + todo_title + NEWLINE)
                 for t in todo_list:
-                    md.write(t + "\n")
+                    md.write(t + NEWLINE)
                 # new line
-                md.write("\n")
+                md.write(NEWLINE)
 
 
 def add_md_top(repo, md, me):
@@ -103,28 +156,45 @@ def add_md_top(repo, md, me):
     with open(md, "a+", encoding="utf-8") as md:
         md.write("## 置顶文章\n")
         for issue in top_issues:
-            if isMe(issue, me):
+            if is_me(issue, me):
                 add_issue_info(issue, md)
         md.write(NEWLINE)
 
+def add_md_firends(repo, md, me):
+    s = FRIENDS_TABLE_HEAD
+    friends_issues = list(repo.get_issues(labels=FRIENDS_LABELS))
+    for issue in friends_issues:
+        for comment in issue.get_comments():
+            if is_hearted_by_me(comment, me):
+                try:
+                    s += _make_friend_table_string(comment.body)
+                except Exception as e:
+                    print(str(e))
+                    pass
+    with open(md, "a+", encoding="utf-8") as md:
+        md.write("## 友情链接\n")
+        md.write(s)
 
-def add_md_recent(repo, md, me):
-    new_five_issues = repo.get_issues()[:5]
+
+def add_md_recent(repo, md, me, limit=5):
+    count = 0
     with open(md, "a+", encoding="utf-8") as md:
         # one the issue that only one issue and delete (pyGitHub raise an exception)
         try:
             md.write("## 最近更新\n")
-            for issue in new_five_issues:
-                if isMe(issue, me):
+            for issue in repo.get_issues():
+                if is_me(issue, me):
                     add_issue_info(issue, md)
-            md.write(NEWLINE)
+                    count += 1
+                    if count >= limit:
+                        break
         except:
             return
 
 
-def add_md_header(md):
+def add_md_header(md, repo_name):
     with open(md, "w", encoding="utf-8") as md:
-        md.write(MD_HEAD)
+        md.write(MD_HEAD.format(repo_name=repo_name))
         md.write(NEWLINE)
 
 
@@ -136,39 +206,37 @@ def add_md_tail(md):
 
 def add_md_label(repo, md, me):
     labels = get_repo_labels(repo)
+
+    # sort lables by description info if it exists, otherwise sort by name, 
+    # for example, we can let the description start with a number (1#Java, 2#Docker, 3#K8s, etc.)
+    labels = sorted(labels, key=lambda x: (x.description is None, x.description == "", x.description, x.name))
+
     with open(md, "a+", encoding="utf-8") as md:
         for label in labels:
 
             # we don't need add top label again
-            if label.name in TOP_ISSUES_LABELS:
-                continue
-
-            # we don't need add todo label again
-            if label.name in TODO_ISSUES_LABELS:
+            if label.name in IGNORE_LABELS:
                 continue
 
             issues = get_issues_from_label(repo, label)
             if issues.totalCount:
-                md.write("## " + label.name + "\n")
+                md.write("## " + label.name + NEWLINE)
                 issues = sorted(issues, key=lambda x: x.created_at, reverse=True)
             i = 0
             for issue in issues:
                 if not issue:
                     continue
-                if isMe(issue, me):
-                    if i < ANCHOR_NUMBER:
-                        add_issue_info(issue, md)
-                    elif i == ANCHOR_NUMBER:
+                if is_me(issue, me):
+                    if i == ANCHOR_NUMBER:
                         md.write("<details><summary>显示更多</summary>\n")
-                        md.write("<ul>\n")
-                        add_issue_indetail_info(issue, md)
-                    else:
-                        add_issue_indetail_info(issue, md)
+                        md.write(NEWLINE)
+                    add_issue_info(issue, md)
                     i += 1
             if i > ANCHOR_NUMBER:
-                md.write("</ul>\n")
-                md.write("</details>\n")
-                md.write("\n")
+                md.write("</details>")
+                md.write(NEWLINE)
+                md.write(NEWLINE)
+
 
 
 def get_to_generate_issues(repo, dir_name, issue_number=None):
@@ -185,24 +253,31 @@ def get_to_generate_issues(repo, dir_name, issue_number=None):
         to_generate_issues.append(repo.get_issue(int(issue_number)))
     return to_generate_issues
 
-
-def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
-    user = login(token)
-    me = get_me(user)
-    repo = get_repo(user, repo_name)
-    add_md_header("README.md")
-    # add to readme one by one, change order here
-    for func in [add_md_top, add_md_recent, add_md_label, add_md_todo]:
-        func(repo, "README.md", me)
-
-    add_md_tail("README.md")
-
-    to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
-
-    # save md files to backup folder
-    for issue in to_generate_issues:
-        save_issue(issue, me, dir_name)
-
+def generate_rss_feed(repo, filename, me):
+    generator = FeedGenerator()
+    generator.id(repo.html_url)
+    generator.title(f"RSS feed of {repo.owner.login}'s {repo.name}")
+    generator.author(
+        {"name": os.getenv("GITHUB_NAME"), "email": os.getenv("GITHUB_EMAIL")}
+    )
+    generator.link(href=repo.html_url)
+    generator.link(
+        href=f"https://raw.githubusercontent.com/{repo.full_name}/master/{filename}",
+        rel="self",
+    )
+    for issue in repo.get_issues():
+        if not issue.body or not is_me(issue, me) or issue.pull_request:
+            continue
+        item = generator.add_entry(order="append")
+        item.id(issue.html_url)
+        item.link(href=issue.html_url)
+        item.title(issue.title)
+        item.published(issue.created_at.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        for label in issue.labels:
+            item.category({"term": label.name})
+        body = "".join(c for c in issue.body if _valid_xml_char_ordinal(c))
+        item.content(CDATA(marko.convert(body)), type="html")
+    generator.atom_file(filename)
 
 def save_issue(issue, me, dir_name=BACKUP_DIR):
     md_name = os.path.join(
@@ -213,9 +288,30 @@ def save_issue(issue, me, dir_name=BACKUP_DIR):
         f.write(issue.body)
         if issue.comments:
             for c in issue.get_comments():
-                if isMe(c, me):
+                if is_me(c, me):
                     f.write("\n\n---\n\n")
                     f.write(c.body)
+
+def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
+    user = login(token)
+    me = get_me(user)
+    repo = get_repo(user, repo_name)
+    add_md_header("README.md", repo_name)
+    # add to readme one by one, change order here
+    for func in  [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
+        func(repo, "README.md", me)
+
+    generate_rss_feed(repo, "feed.xml", me)
+
+    add_md_tail("README.md")
+
+    to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
+
+    # save md files to backup folder
+    for issue in to_generate_issues:
+        save_issue(issue, me, dir_name)
+
+
 
 
 if __name__ == "__main__":
