8d8937c7895d6b578dfcf311ed1b0ec0d2cecaae
drunkwretch <drunkwretch@gmail.com>
2023-07-27 19:44:39 +0800

    增加css链接

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ FRIENDS_INFO_DICT = {
     "描述": "",
 }
 
+CSS = """<link rel="stylesheet" href="./style.css" />"""
+
 
 def get_me(user):
     return user.get_user().login
@@ -189,6 +191,10 @@ def add_md_header(md, repo_name):
         md.write(MD_HEAD.format(repo_name=repo_name))
         md.write("\n")
 
+def add_md_tail(md):
+    with open(md, "a+", encoding="utf-8") as md:
+        md.write(CSS)
+        md.write("\n")
 
 def add_md_label(repo, md, me):
     labels = get_repo_labels(repo)
@@ -273,6 +279,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     add_md_header("README.md", repo_name)
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
+    add_md_tail("README.md")
 
     generate_rss_feed(repo, "feed.xml", me)
     to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
